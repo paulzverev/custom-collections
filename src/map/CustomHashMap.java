a88d4daef5e476fd7acf410cf01df4da91be05ae
Paul <paulzvereve@gmail.com>
2026-08-24 20:42:35 +0300

    wrong implementation

--- src/map/CustomHashMap.java
+++ src/map/CustomHashMap.java
@@ -1,11 +1,14 @@
 package map;
 
+import java.util.Objects;
+
 public class CustomHashMap<K, V> {
 
     private Node<K, V>[] buckets;
+
+    private static final int DEFAULT_CAPACITY = 16;
     private int capacity;
     private int size;
-    private static final int DEFAULT_CAPACITY = 16;
 
     @SuppressWarnings("unchecked")
     public CustomHashMap() {
@@ -15,9 +18,10 @@ public class CustomHashMap<K, V> {
 
     private static class Node<K, V> {
 
-        private K key;
+        private final K key;
         private V value;
         private int hashCode;
+        private Node<K, V> next;
 
         private Node(K key, V value, int hashCode) {
             this.key = key;
@@ -27,14 +31,36 @@ public class CustomHashMap<K, V> {
     }
 
     public void put(K key, V value) {
-        int optimizedHashCode = toOptimizedHashCode(key);
-        int bucketIndex = getBucketIndex(optimizedHashCode);
+        int incomingKeyHashCode = toOptimizedHashCode(key);
+        int bucketIndex = getBucketIndex(incomingKeyHashCode);
+        Node<K, V> existing = buckets[bucketIndex];
 
-        buckets[bucketIndex] = new Node<>(key, value, optimizedHashCode);
-        size++;
+        if (existing == null) {
+            buckets[bucketIndex] = new Node<>(key, value, incomingKeyHashCode);
+            size++;
+        } else {
+            if (existing.hashCode == incomingKeyHashCode) {
+                if (Objects.equals(existing.key, key)) {
+                    existing.value = value;
+                }
+            } else {
+                Node<K, V> current = buckets[bucketIndex];
+
+                while (current.next != null) {
+                    current = current.next;
+                }
+
+                current.next = new Node<>(key, value, incomingKeyHashCode);
+                size++;
+            }
+        }
     }
 
     private static int toOptimizedHashCode(Object o) {
+        if (o == null) {
+            return 0;
+        }
+
         int objectHashCode = o.hashCode();
         return objectHashCode ^ (objectHashCode >>> 16);
     }
---
+++ src/map/Main.java
@@ -0,0 +1,8 @@
+package map;
+
+public class Main {
+    public static void main(String[] args) {
+        CustomHashMap<String, Integer> map = new CustomHashMap<>();
+        map.put("Paul", 19);
+    }
+}
